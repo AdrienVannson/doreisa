c36d881545f0ab8305542cf3a9cc488c11cd555b
Adrien Vannson <adrien.vannson@protonmail.com>
2025-03-03 17:57:15 +0100

    Refactor the client

--- doreisa/simulation_node.py
+++ doreisa/simulation_node.py
@@ -1,29 +1,45 @@
 import numpy as np
 import ray
+from dataclasses import dataclass
+
+
+@dataclass
+class _Chunk:
+    array_name: str
+    chunk_position: tuple[int, ...]
 
 
 class Client:
     """
     Used by the MPI nodes to send data to the analytic cluster.
 
-    The client is in charge of a single chunk of data. It will send it at each timestep.
+    The client is in charge of a several chunks of data. Each chunk is at a certain position in an
+    array.
     """
 
-    def __init__(self, array_name: str, chunk_position: tuple[int, ...]) -> None:
+    def __init__(self) -> None:
         self.head = ray.get_actor("simulation_head", namespace="doreisa")
 
         self.timestep = 0
-        self.array_name = array_name
-        self.chunk_position = chunk_position
 
-    def create_array(self, name: str, nb_chunks_per_dim: tuple[int, ...]) -> None:
+        # The chunks that the client is in charge of updating
+        self.chunks: list[_Chunk] = []
+
+    def create_array(self, name: str, nb_chunks_per_dim: tuple[int, ...], chunk_position: tuple[int, ...]) -> None:
         self.head.create_array.remote(name, nb_chunks_per_dim)
 
-    def simulation_step(self, temperatures: np.ndarray) -> None:
-        ref = ray.put(temperatures)
+        self.chunks.append(_Chunk(name, chunk_position))
 
-        future = self.head.add_chunk.remote(self.array_name, self.timestep, self.chunk_position, [ref])
+    def simulation_step(self, chunks: list[np.ndarray]) -> None:
+        assert len(chunks) == len(self.chunks)
+
+        for chunk, chunk_info in zip(chunks, self.chunks):
+            ref = ray.put(chunk)
+
+            future = self.head.add_chunk.remote(chunk_info.array_name, self.timestep, chunk_info.chunk_position, [ref])
+
+            # Wait until the data is processed before returning to the simulation
+            # TODO the synchronization is not that good
+            ray.get(future)
+
         self.timestep += 1
-
-        # Wait until the data is processed before returning to the simulation
-        ray.get(future)
