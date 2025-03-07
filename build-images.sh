9829bd3c6654d8d798293d7c6bd7a533f622590e
Adrien Vannson <adrien.vannson@protonmail.com>
2025-03-07 18:23:52 +0100

    Convert the images to singularity

--- build-images.sh
+++ build-images.sh
@@ -4,5 +4,9 @@ docker build --pull --rm -f 'docker/simulation/Dockerfile' -t 'doreisa-simulatio
 
 # Export the docker images to a .tar file
 mkdir -p docker/images
-docker save doreisa-analytics:latest -o dcker/images/doreisa-analytics.tar
+docker save doreisa-analytics:latest -o docker/images/doreisa-analytics.tar
 docker save doreisa-simulation:latest -o docker/images/doreisa-simulation.tar
+
+# Convert the images to singularity images
+singularity build docker/images/doreisa-analytics.sif docker-archive://docker/images/doreisa-analytics.tar
+singularity build docker/images/doreisa-simulation.sif docker-archive://docker/images/doreisa-simulation.tar
