8f8ac38aaaab71ef1a9f0846f266a4204664ccaf
Adrien Vannson <adrien.vannson@protonmail.com>
2025-03-10 16:48:20 +0100

    Remove useless command

--- build-images.sh
+++ build-images.sh
@@ -3,7 +3,6 @@ docker build --pull --rm -f 'docker/analytics/Dockerfile' -t 'doreisa-analytics:
 docker build --pull --rm -f 'docker/simulation/Dockerfile' -t 'doreisa-simulation:latest' 'docker/simulation'
 
 # Export the docker images to a .tar file
-mkdir -p docker/images
 docker save doreisa-analytics:latest -o docker/images/doreisa-analytics.tar
 docker save doreisa-simulation:latest -o docker/images/doreisa-simulation.tar
 
