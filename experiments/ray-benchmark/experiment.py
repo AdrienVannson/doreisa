40eea0d995beea4311561c9cf3fb2b4ce320f4df
Adrien Vannson <adrien.vannson@protonmail.com>
2025-04-16 12:25:31 +0200

    Add ray benchmark

---
+++ experiments/ray-benchmark/experiment.py
@@ -0,0 +1,52 @@
+import execo
+import execo_g5k
+import execo_engine
+import time
+
+JOB_ID, SITE = ..., ...
+
+
+def run_experiment(nb_reserved_nodes: int, nb_actors: int, nb_iterations: int) -> None:
+    # Get useful stats
+    nodes = execo_g5k.get_oar_job_nodes(JOB_ID, SITE, timeout=None)
+    head_node, nodes = nodes[0], nodes[1:]
+
+    print(head_node, nodes, flush=True)
+
+    # Stop ray everywhere
+    stops = []
+    for node in nodes + [head_node]:
+        stop_ray = execo.SshProcess(
+            f"""singularity exec doreisa/docker/images/doreisa-simulation.sif bash -c "ray stop" """,
+            node,
+        )
+        stop_ray.start()
+        stops.append(stop_ray)
+
+    for stop in stops:
+        stop.wait()
+
+    # Start the head node
+    # It is important to set the ulimit to a high value, otherwise, the simulation will be stuck
+    head_node_cmd = execo.SshProcess(
+        f'ulimit -n 65535; singularity exec doreisa/docker/images/doreisa-simulation.sif bash -c "cd doreisa; ray start --head --port=4242; python3 experiments/ray-benchmark/head.py {nb_actors} {nb_iterations}"',
+        head_node,
+    )
+    head_node_cmd.start()
+
+    time.sleep(2)
+
+    # Start the worker nodes
+    for node in nodes:
+        print("Starting node ", node, flush=True)
+        node_cmd = execo.SshProcess(
+            f"""ulimit -n 65535; singularity exec doreisa/docker/images/doreisa-simulation.sif bash -c "ray start --address='{head_node.address}:4242'"; sleep infinity """,
+            node,
+        )
+        node_cmd.start()
+
+    head_node_cmd.wait()
+
+for nb_actors in [1, 2, 4, 8, 16]:
+    for nb_iterations in [1024, 2048, 4096, 8192, 16384, 32768, 65536]:
+        run_experiment(10, nb_actors, nb_iterations)
---
+++ experiments/ray-benchmark/head.py
@@ -0,0 +1,61 @@
+import time
+import random
+import sys
+
+import ray
+from ray.util.scheduling_strategies import NodeAffinitySchedulingStrategy
+import numpy as np
+
+NB_ACTORS = int(sys.argv[1])
+NB_ITERATIONS = int(sys.argv[2])
+
+# Wait until all the nodes have joined the cluster
+time.sleep(4)
+
+ray.init()
+
+assert len(ray.nodes()) == 10
+
+@ray.remote
+def create_big_array():
+    return ray.put(np.random.random((1000, 1000)))
+
+@ray.remote
+def identity(x):
+    return x
+
+refs = []
+for node in ray.nodes():
+    refs.append(create_big_array.options(
+        scheduling_strategy=NodeAffinitySchedulingStrategy(
+            node_id=node["NodeID"],
+            soft=False,
+        ),
+    ).remote())
+
+@ray.remote
+class SchedulingActor:
+    def __init__(self, refs: list[ray.ObjectRef]):
+        self.refs = refs
+
+    def start_tasks(self):
+        futures = []
+        for _ in range(NB_ITERATIONS):
+            ref = random.choice(self.refs)
+            futures.append(identity.remote(ref))
+
+        ray.get(futures)
+
+scheduling_actors = [SchedulingActor.options(
+    scheduling_strategy=NodeAffinitySchedulingStrategy(
+        node_id=ray.get_runtime_context().get_node_id(),
+        soft=False,
+    )
+).remote(refs) for _ in range(NB_ACTORS)]
+
+begin = time.time()
+ray.get([a.start_tasks.remote() for a in scheduling_actors])
+end = time.time()
+
+with open("ray-benchmark.txt", "a") as f:
+    f.write(f"{NB_ACTORS} {NB_ITERATIONS} {end - begin}\n")
