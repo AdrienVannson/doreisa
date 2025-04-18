d377657276f8743d60d6bf30a9ad3e3c497fa154
Adrien Vannson <adrien.vannson@protonmail.com>
2025-04-18 16:48:26 +0200

    Add scheduling actor file

---
+++ doreisa/_scheduling_actor.py
@@ -0,0 +1,11 @@
+import ray
+
+
+@ray.remote
+class SchedulingActor:
+    """
+    Actor in charge of gathering ObjectRefs and scheduling the tasks produced by the head node.
+    """
+
+    def __init__(self) -> None:
+        self.head = ray.get_actor("simulation_head", namespace="doreisa")
